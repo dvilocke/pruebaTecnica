f0258982109ba719422b178fc20ab481b217df8f
Miguel <miguel.ramirez@ohmyfi.com>
2023-02-03 09:53:42 -0500

    finalizado

--- app/home.py
+++ app/home.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, url_for, request, redirect, flash, session
 from app.auth import login_required
 import requests
-import random
+import os
+import json
+import datetime
 bp_menu = Blueprint('home', __name__)
 
 api = "70489895cb00438eb17a94daf6c20b15"
@@ -9,12 +11,33 @@ api = "70489895cb00438eb17a94daf6c20b15"
 @bp_menu.route('/')
 @login_required
 def home():
+
+    ROUTE = "app/db/consumos.json"
+    if not os.path.exists(ROUTE):
+        with open(ROUTE, "w") as f:
+            f.close()
+
     #Hacemos la petición
     url = ('https://newsapi.org/v2/top-headlines?'
            'pageSize=4&'
            'sources=abc-news&'
            'apiKey=70489895cb00438eb17a94daf6c20b15')
     response = requests.get(url)
+
+    if response.status_code == 200:
+        #Logica para guardar el consumo
+        with open(ROUTE, "r") as f:
+            file = json.loads(f.read())
+            f.close()
+
+        new_file = file.append({
+            datetime.datetime.now() : "https://newsapi.org/v2/top-headlines?"
+        })
+
+        with open(ROUTE, "w") as f:
+            f.write(json.dumps(new_file))
+            f.close()
+
     response = response.json()["articles"]
     return render_template("notice.html", notices = response)
 
